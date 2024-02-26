677d5fcd3b5fb9a3cde104b99d47f0c280e8d065
xiaodiode <xgloriaxiaox@gmail.com>
2024-02-26 16:35:43 -0600

    timer can count down, added timer UI

---
+++ Mortal Mansion/Assets/Scripts/Time/Timer.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Time (in seconds) For Each Cycle")] 
+    [SerializeField] public int dayBase, nightBase;
+    [SerializeField] public int currDayTime, currNightTime;
+    [SerializeField] public int timeIncrement; // how many seconds to increment both timers after each round
+
+    [SerializeField] private DayController dayController; 
+    [SerializeField] private int totalSeconds;
+
+    [Header("UI Timer")]
+    [SerializeField] private TextMeshProUGUI time;
+
+    private string leadingZeroH, leadingZeroM, leadingZeroS;
+    private string timeText = "00:00:00";
+    private bool countDown = false;
+    private int hours, minutes, seconds;
+    private int secondsStarted, secondsPassed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        leadingZeroH = "0";
+        leadingZeroM = "0";
+        leadingZeroS = "0";
+
+        currDayTime = dayBase;
+        currNightTime = nightBase;
+
+        totalSeconds = currDayTime;
+
+        startCountDown(); //delete this later. will need to start counting down after player starts game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countDown){
+            updateTimerUI();
+        }
+        
+    }
+
+    public void updateTimerUI(){
+        if(countDown){
+            secondsPassed = totalSeconds - (Mathf.FloorToInt(Time.time) - secondsStarted);
+            seconds = secondsPassed % 60;
+            minutes = (secondsPassed/60) % 60;
+            if(minutes > 9){
+                leadingZeroM = "";
+            }
+            else{
+                leadingZeroM = "0";
+            }
+            if(seconds > 9){
+                leadingZeroS = ":";
+            }
+            else{
+                leadingZeroS = ":0";
+            }
+            if(secondsPassed == 0){
+                // levelClear = true;
+                // if(player.selectedLevel == player.levelsUnlocked){
+                //     gemController.enableGemPopup("level");
+                //     if(player.selectedLevel != 101)
+                //         player.levelsUnlocked+=1;
+                // }
+                // gameManager.gameOver();
+                dayController.switchCycles();
+                resetTimer();
+            }
+            timeText = leadingZeroM + minutes.ToString() + leadingZeroS + seconds.ToString();
+            
+        }
+        
+        time.text = timeText;
+    }
+
+    public void increaseTime(){
+        currDayTime += timeIncrement;
+        currNightTime += timeIncrement;
+    }
+
+    public void startCountDown(){
+        secondsStarted = Mathf.FloorToInt(Time.time);
+        countDown = true;
+    }
+
+    public void pauseTimer(){
+        countDown = false;
+    }
+    public void resumeTimer(){
+        countDown = true;
+    }
+    public void resetTimer(){
+        countDown = false;
+        
+        if(dayController.isNight){
+            totalSeconds = currNightTime;
+        }
+        else{
+            totalSeconds = currDayTime;
+        }
+    }
+
+
+    public int getTime(){
+        return Mathf.FloorToInt(Time.time);
+    }
+    public int getGameTime(){
+        return secondsPassed;
+    }
+
+    
+}
